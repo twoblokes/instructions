d5f41671538fb8fe1edde84ea7fb85a5680819e0
Tom Sansome <tom@sanso.me>
2013-12-29 20:42:40 +0000

    First commit

---
+++ a000_test/index.php
@@ -0,0 +1,71 @@
+<?php 
+
+  include '../includes/header.php';
+
+  // Edit these variables per project
+  $clientName        = 'Test Client';
+  $clientsite        = 'http://testclient.com';
+
+?>
+  
+    <title><?php echo $clientName; ?> | Client Instructions | Two Blokes</title>
+
+  </head>
+
+  <body>
+
+    <header class="main-header">
+      <h1 class="client-name"><?php echo $clientName; ?></h1>
+      <h1 class="client-name-again"><?php echo $clientName; ?></h1>
+    </header>
+
+    <?php include '../includes/sidebar.php'; ?>
+
+    <section>
+      <header>
+        <h2>Login</h2>
+      </header>
+      <main>
+        <ul>
+          <li>Go to this url - <a href="<?php echo $clientsite ?>/wp-admin"><?php echo $clientsite ?>/wp-admin</a></li>
+          <li>Username: <strong><?php echo $clientName ?></strong></li>
+          <li>Password: <strong>password</strong></li>
+        </ul>
+      </main>
+    </section>
+
+    <section>
+      <header>
+        <h2>Add Post</h2>
+      </header>
+      <main>
+        <p><span class="alert">Sentance here, something you may need to say before or after a list. Say before or after a list.</span></p>
+        <ul>
+          <li>Login, click '<strong>Posts</strong>' in the left menu.</li>
+          <li>Click '<strong>Add New</strong>' at the top.</li>
+          <li><span class="alert">Here is a text alert. Something important to tell the user? Use span class 'alert'.</span></li>
+        </ul>
+      </main>
+    </section>
+
+    <section>
+      <header>
+        <h2>Edit Post</h2>
+      </header>
+      <main>
+        <p><span class="alert">Sentance here</span>, something you may need to say before or after a list. Say before or after a list.</p>
+        <ul>
+          <li>Login, click '<strong>Posts</strong>' in the left menu.</li>
+          <li>Click '<strong>Add New</strong>' at the top.</li>
+        </ul>
+      </main>
+    </section>
+
+    <section>
+      <header>
+        <h2>Outro</h2>
+      </header>
+      <p>Just a text card will work nicely too bro. Just a text card will work nicely too bro. Just a text card will work nicely too bro. Just a text card will work nicely too bro. Just a text card will work nicely too bro. Just a text card will work nicely too bro. Just a text card will work nicely too bro. </p>
+    </section>
+
+<?php include '../includes/footer.php'; ?>
---
+++ instructions-for-instructions.md
@@ -0,0 +1,11 @@
+# Instructions for instructions
+
+Setup:
+
+1. Clone example (a000_test) into clean directory
+```
+git clone git@github.com:twoblokes/instructions.git
+```
+2. Rename a006_test to client number and name
+3. Open .htaccess and rename AuthUserFile with client number and name
+4. Open .htpasswd and rename username to client name and password using [http://davidwalsh.name/web-development-tools](http://davidwalsh.name/web-development-tools)
